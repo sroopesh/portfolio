12559f3bdd9429a28ca7188f0e2681e33e4d2e58
SRoopesh <roopeshmahi.7@gmail.com>
2025-07-08 14:54:22 +0530

    New resume added and updated in resumeComponent

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -10,7 +10,7 @@ export default function Resume() {
     >
       <h2 className="text-4xl font-semibold mb-5 text-white">RESUME</h2>
       <a
-        href="/Roopesh_Srinivasa_Resume_2025.pdf"
+        href="/Roopesh_Srinivasa_FullStackDeveloper_Resume.pdf"
         download
         className="inline-flex items-center gap-3 bg-gradient-to-r from-purple-500 to-pink-500 text-white px-6 py-3 rounded-full shadow-lg hover:scale-105 hover:from-pink-500 hover:to-purple-500 transition-all duration-300"
       >
